552edff3fa2b4e93eb3fc62e56742812b4a31c44
Santiago Torres <santiagotopo@unisabana.edu.co>
2026-03-23 19:55:51 -0500

    Proyectos MAVEN (SpringBoot)

---
+++ player-service/src/test/java/com/actividad/player_service/PlayerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.actividad.player_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
